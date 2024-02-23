b8ce6b8dfd09967f6c76626f2edc05eea1c8b4b4
Pushp Singh <pushp_singh@condenast.com>
2024-02-23 22:25:53 +0530

    added functionality to intor input fields

--- src/Components/Section/Intro/IntroductionComponent.jsx
+++ src/Components/Section/Intro/IntroductionComponent.jsx
@@ -1,55 +1,88 @@
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Unstable_Grid2";
 import { useState } from "react";
 
 import { TextField } from "../../TextField/TextField";
-import {TextArea}from '../../TextArea/TextArea';
+import { TextArea } from "../../TextArea/TextArea";
 import "./style.css";
 
 const IntroductionComponent = () => {
-  const [profilePhoto, setProfilePhoto] = useState(null);
+  const [introSectionData, setIntroSectionData] = useState({
+    siteTitle: "",
+    profilePhoto: null,
+    profileTitle: "",
+    profileSubTitle: "",
+    profileName: "",
+    profileEmail: "",
+  });
+
+  const handleInputChange = (event) => {
+    event.stopPropagation();
+
+    const { name, value } = event.target;
+    setIntroSectionData({ ...introSectionData, [name]: value });
+  };
+
   const uploadPhoto = (event) => {
     event.preventDefault();
-
+    
+    const { name } = event.target;
     const file = URL.createObjectURL(event.target.files[0]);
-    setProfilePhoto(file);
+    setIntroSectionData({ ...introSectionData, [name]: file });
   };
 
   return (
     <Grid container spacing={2} className="parent-container">
       <Grid xs={12} sm={12} md={12}>
         <div>
-          <TextField className="site-title" placeholder="Enter site title" />
+          <TextField
+            className="site-title"
+            name="siteTitle"
+            placeholder="Enter site title"
+            value={introSectionData.siteTitle}
+            onChange={handleInputChange}
+          />
         </div>
       </Grid>
       <Grid xs={12} sm={2} md={4}>
         <div
           className="profile-photo"
-          onChange={uploadPhoto}
+          name="profilePhoto"
           style={{
-            backgroundImage: `url(${profilePhoto})`,
+            backgroundImage: `url(${introSectionData.profilePhoto})`,
             backgroundSize: "100% auto",
           }}
         >
           <input
             type="file"
             id="imagefile"
-            name="imagefile"
+            name="profilePhoto"
             onChange={uploadPhoto}
           />
         </div>
       </Grid>
-      <Grid xs={12} sm={5} md={4} className="profile-title-container" smOffset={3} mdOffset={0}>
+      <Grid
+        xs={12}
+        sm={5}
+        md={4}
+        className="profile-title-container"
+        smOffset={3}
+        mdOffset={0}
+      >
         <div className="profile-title-subtitle">
           <TextArea
             className="profile-title"
+            name="profileTitle"
             placeholder="Click to add title"
+            value={introSectionData.profileTitle}
+            onChange={handleInputChange}
             rows="3"
           />
           <TextArea
             className="profile-subtitle"
+            name="profileSubTitle"
             placeholder="Click to add subtitle"
+            value={introSectionData.profileSubTitle}
+            onChange={handleInputChange}
             rows="3"
           />
         </div>
@@ -58,14 +91,21 @@ const IntroductionComponent = () => {
         <div className="profile-name-email">
           <TextField
             className="profile-name"
+            name="profileName"
             placeholder="Your name here"
+            value={introSectionData.profileName}
+            onChange={handleInputChange}
           />
           <TextField
             className="profile-email"
+            name="profileEmail"
             placeholder="Enter email"
+            value={introSectionData.profileEmail}
+            onChange={handleInputChange}
           />
         </div>
       </Grid>
+      <button onClick={populateData}>CHECK ALL DATA</button>
     </Grid>
   );
 };
