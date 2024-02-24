163f93d3c03006fbcb87dab4ed1a2e6da2dc9026
Pushp Singh <pushp_singh@condenast.com>
2024-02-24 17:42:22 +0530

    added style for section

--- src/Page/Edit-page/index.jsx
+++ src/Page/Edit-page/index.jsx
@@ -1,6 +1,7 @@
-import React, {useState, Suspense} from 'react';
-import {MenuPopUpButton} from '../../Components/MenuPopup/MenuPopup';
+import React, { useState, Suspense } from "react";
+import { MenuPopUpButton } from "../../Components/MenuPopup/MenuPopup";
 
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 
 import IntroductionComponent from "../../Components/Section/Intro/IntroductionComponent";
@@ -10,7 +11,9 @@ const ProjectComponent =  React.lazy(() =>import("../../Components/Section/Proje
 const ExperienceComponent =  React.lazy(() =>import("../../Components/Section/Experience/ExperienceComponent"));
 const CTAComponent =  React.lazy(() =>import("../../Components/Section/CTA/CTAComponent"));
 
-import {MenuPopUpContext} from '../context';
+import { MenuPopUpContext } from "../context";
+
+import './style.css';
 
 const EditPage = () => {
   const [showSection, setShowSection] = useState({
@@ -20,26 +23,24 @@ const EditPage = () => {
     experience: false,
     cta: false,
   });
-  
+
   return (
     <Container maxWidth="xl">
-      <IntroductionComponent/>
-      <Suspense>
-        {showSection.aboutMe && <AboutMeComponent />}
-      </Suspense>
-      <Suspense>
-        {showSection.skillSet && <SkillSetComponent />}
-      </Suspense>
-      <Suspense>
-        {showSection.projects && <ProjectComponent />}
-      </Suspense>
-     <Suspense>
-      {showSection.experience &&  <ExperienceComponent />}
-     </Suspense>
-      <Suspense>
-        {showSection.cta && <CTAComponent />}
-      </Suspense>
-      <MenuPopUpContext.Provider value={{showSection, setShowSection}}>
+      <IntroductionComponent />
+      <Container fixed spacing={2}>
+        <Box className="section-container" sx={{
+               bgcolor: '#cfe8fc', width: 'auto', display: 'flex', flexDirection: 'column',  alignItems: 'center'}} >
+          <Suspense>{showSection.aboutMe && <AboutMeComponent />}</Suspense>
+          <Suspense>{showSection.skillSet && <SkillSetComponent />}</Suspense>
+          <Suspense>{showSection.projects && <ProjectComponent />}</Suspense>
+          <Suspense>
+            {showSection.experience && <ExperienceComponent />}
+          </Suspense>
+          <Suspense>{showSection.cta && <CTAComponent />}</Suspense>
+        </Box>
+      </Container>
+
+      <MenuPopUpContext.Provider value={{ showSection, setShowSection }}>
         <MenuPopUpButton />
       </MenuPopUpContext.Provider>
     </Container>
--- src/Page/Public-Page/index.jsx
+++ src/Page/Public-Page/index.jsx
@@ -1,6 +1,10 @@
+import {React, useContext} from 'react';
+import {MenuPopUpContext} from '../context';
 
 
 const PublicPage = () => {
+    const {showSection} = useContext(MenuPopUpContext);
+    console.log('public view', showSection);
     return(
         <h3>Public view Page</h3>
     )
