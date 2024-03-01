1913ee04c32e37bb0b6a77da810afdc51908156c
Pushp Singh <pushp_singh@condenast.com>
2024-03-01 16:58:55 +0530

    added modal box using bootstrap

--- src/Components/Modal-Box/Modal.jsx
+++ src/Components/Modal-Box/Modal.jsx
@@ -1,72 +1,51 @@
-import React, { Suspense } from "react";
+import React, { useState, Suspense } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+import Modal from "react-bootstrap/Modal";
 
 const AboutMeComponent = React.lazy(() => import("../../Components/Section/AboutMe/AboutMeComponent"));
 const IntroductionComponent = React.lazy(() => import("../../Components/Section/Intro/IntroductionComponent"));
 const SkillSetComponent =  React.lazy(() => import("../../Components/Section/SkillSet/SkillSetComponent"));
 
-import EditPage from "../../Page/Edit-page/index";
-
-import './style.css';
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "80%",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-//   width: "auto",
-//   display: "flex",
-//   flexDirection: "column",
-//   alignItems: "flex-start",
-};
+import "./style.css";
 
 const ModalBox = ({ introSectionData }) => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [lgShow, setLgShow] = useState(false);
 
   return (
     <div>
-      <Button
-        variant="contained"
-        className="preview-page-btn"
-        onClick={handleOpen}
-      >
-        Preview
-      </Button>
+      <Button onClick={() => setLgShow(true)}>Preview</Button>
+
       <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        size="xl"
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
       >
-        <Container maxWidth="xl">
-          <Suspense>
-            <IntroductionComponent {...{ introSectionData }} />
-          </Suspense>
-          <Container fixed spacing={2}>
-            <Box
-              className="section-container"
-              sx={style}
-            >
-                {/* <Suspense>
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Preview Mode
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container maxWidth="xl">
+            <Suspense>
+              <IntroductionComponent {...{ introSectionData }} />
+            </Suspense>
+            <Container fixed spacing={2}>
+              <Box className="section-container">
+                <Suspense>
                     <AboutMeComponent />
                 </Suspense>
                 <Suspense>
                     <SkillSetComponent />
-                </Suspense> */}
-            </Box>
+                </Suspense>
+              </Box>
+            </Container>
           </Container>
-        </Container>
+        </Modal.Body>
       </Modal>
     </div>
   );
@@ -74,4 +53,3 @@ const ModalBox = ({ introSectionData }) => {
 
 export default ModalBox;
 
-// TODO: make sure Modal is responsive
